81a6c4dcd4db558d4eeb47d3a9b7ff541dc5a7b3
sagnik3788 <sagnikdas5432@gmail.com>
2025-06-10 19:32:02 +0530

    testing
    Signed-off-by: sagnik3788 <sagnikdas5432@gmail.com>

--- pkg/app/pipedv1/plugin/opentofu/deployment/testdata/simple/main.tf
+++ pkg/app/pipedv1/plugin/opentofu/deployment/testdata/simple/main.tf
@@ -1,5 +1,18 @@
 terraform {
   required_version = ">= 1.0.0"
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+}
+
+provider "random" {}
+
+resource "random_string" "test" {
+  length  = 8
+  special = false
 }
 
 resource "null_resource" "test" {}
